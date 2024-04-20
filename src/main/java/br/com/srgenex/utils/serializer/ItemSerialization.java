7383df77abc4d2f6ce6f1e0248d35674ccc3097f
GeneX <unconfigured@null.spigotmc.org>
2024-04-20 15:12:57 -0300

    Updates

--- src/main/java/br/com/srgenex/utils/serializer/ItemSerialization.java
+++ src/main/java/br/com/srgenex/utils/serializer/ItemSerialization.java
@@ -38,4 +38,28 @@ public class ItemSerialization {
         }
     }
 
+    public static String serialize(ItemStack item) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+            dataOutput.writeObject(item);
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
+
+    public static ItemStack deserializeItem(String data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack item = (ItemStack) dataInput.readObject();
+            dataInput.close();
+            return item;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
+    }
+
 }
