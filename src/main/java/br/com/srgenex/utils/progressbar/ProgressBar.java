59d7ebef97a03c740d49202c2eca8c7ab22bd3f5
GeneX <unconfigured@null.spigotmc.org>
2025-09-05 21:56:40 -0300

    Added double generate progressbar method

--- src/main/java/br/com/srgenex/utils/progressbar/ProgressBar.java
+++ src/main/java/br/com/srgenex/utils/progressbar/ProgressBar.java
@@ -27,4 +27,27 @@ public class ProgressBar {
         return progressBar.toString();
     }
 
+    public static String generateProgressBar(double currentValue, double totalValue, int barLength, String completedColor, String uncompletedColor, String barSymbol) {
+        double progress = currentValue / totalValue;
+
+        int completedBars = (int) (progress * barLength);
+        int uncompletedBars = barLength - completedBars;
+
+        StringBuilder progressBar = new StringBuilder();
+        progressBar.append(completedColor);
+
+        for (int i = 0; i < completedBars; i++) {
+            progressBar.append(barSymbol);
+        }
+
+        progressBar.append(uncompletedColor);
+
+        for (int i = 0; i < uncompletedBars; i++) {
+            progressBar.append(barSymbol);
+        }
+
+        progressBar.append(ChatColor.RESET);
+        return progressBar.toString();
+    }
+
 }
